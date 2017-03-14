5a4f5c9cf3534f84615eb07db2dafead7657f2e7
cari1928 <13030622@itcelaya.edu.mx>
2017-03-13 20:05:00 -0600

    Funcionalidad Giro - Musica

--- app/src/main/java/com/example/radog/a2017_03_13_patm_verdatos/MainActivity.java
+++ app/src/main/java/com/example/radog/a2017_03_13_patm_verdatos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.radog.a2017_03_13_patm_verdatos;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             //objM = MediaPlayer.create(this, R.raw.guren_no_yumiya_violin);
             objM = MediaPlayer.create(this, R.raw.guren_no_yumiya);
             objM.start();
+
         } else if(!objM.isPlaying()) {
             objM.seekTo(pause);
             objM.start();
@@ -55,29 +57,53 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnPause)
     public void btnPause(){
-        objM.pause();
-        pause = objM.getCurrentPosition();
+        if(objM != null) {
+            objM.pause();
+            pause = objM.getCurrentPosition();
+        }
     }
 
     @OnClick(R.id.btnStop)
     public void btnStop() {
-        objM.release();
-        objM = null;
+        if(objM != null) {
+            objM.release();
+            objM = null;
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle datos) {
+        int position;
+
         //guardar datos
         datos.putString("NOM",  etNombre.getText().toString());
         datos.putString("TEL", etTelefono.getText().toString());
         datos.putString("EMA", etEmail.getText().toString());
+
+        if(objM != null) { //verifica que exista un objeto MediaPlayer
+            //guarda hasta donde se está reproduciendo
+            position = objM.getCurrentPosition();
+            datos.putInt("POS", position);
+            objM.pause(); //pausa la música, se reanuda en onRestoreInstanceState
+        }
+
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle datos) {
+        int position;
+
         //datos recuperados
          etNombre.setText(datos.getString("NOM"));
         etTelefono.setText(datos.getString("TEL"));
         etEmail.setText(datos.getString("EMA"));
+
+        //el objeto se vuelve null... no sé porqué
+        if(datos != null && objM == null) {
+            objM = MediaPlayer.create(this, R.raw.guren_no_yumiya); //vuelvo a crear el objeto
+            position = datos.getInt("POS"); //recupero la posición en donde se quedó
+            objM.seekTo(position); //voy a esa posición
+            objM.start(); //restauro la música
+        }
     }
 }
